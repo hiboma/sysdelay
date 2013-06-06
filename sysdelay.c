d9cedc85a8d68a72bee6a1edb5e5492f5ea4aca6
id:hiboma(ひろや) <hiroyan@gmail.com>
2013-06-06 19:17:28 +0900

    initial commit

---
+++ sysdelay.c
@@ -0,0 +1,200 @@
+#include "sysdelay.h"
+
+void attach(pid_t tid) {
+    int rc = ptrace(PTRACE_ATTACH, tid, 0, 0);
+    if (rc == -1) {
+        err(1, "failed to ptrace(PTRACE_ATTACH, %d, ..)");
+    }
+
+    thread *t = malloc(sizeof(thread));
+    if (t == NULL) {
+        err(1, "failed to malloc");
+    }
+    t->tid        = tid;
+    t->in_syscall = false;
+    HASH_ADD_INT(threads_attached, tid, t);
+}
+
+void detach(pid_t pid) {
+    int rc = ptrace(PTRACE_DETACH, pid, 0, 0);
+    if (rc == -1) {
+        err(1, "failed to ptrace(PTRACE_DETACH, %d, ..)", pid);
+    }
+}
+
+thread * find_attached_thread(pid_t tid) {
+    thread *t;
+    HASH_FIND_INT(threads_attached, &tid, t);
+    return t;
+}
+
+/* from strace.c */
+static void attach_all_threads(pid_t pid) {
+
+    char procdir[sizeof("/proc/%d/task") + sizeof(int) * 3];
+    DIR *dir;
+
+    sprintf(procdir, "/proc/%d/task", pid);
+    dir = opendir(procdir);
+    if (dir != NULL) {
+        unsigned int ntid = 0, nerr = 0;
+        struct dirent *de;
+
+        while ((de = readdir(dir)) != NULL) {
+            struct tcb *cur_tcp;
+            int tid;
+
+            if (de->d_fileno == 0)
+                continue;
+            /* we trust /proc filesystem */
+            tid = atoi(de->d_name);
+            if (tid <= 0)
+                continue;
+            attach(tid);
+        }
+        closedir(dir);
+    }
+}
+
+bool ignored_syscall(unsigned int syscall) {
+    switch(syscall) {
+    case SYS_futex:
+    case SYS_nanosleep:
+    case SYS_gettimeofday:
+    case SYS_clock_gettime:
+    case SYS_pselect6:
+    case SYS_epoll_wait:
+    case SYS_sched_yield:
+    case SYS_restart_syscall:
+    case SYS_rt_sigaction:
+    case SYS_rt_sigprocmask:
+        return true;
+    default:
+        return false;
+    }
+}
+
+const char * syscall_name(long rax) {
+    return sysent[rax];
+}
+
+static void signal_handler(int sig) {
+   _got_signal = true;
+}
+
+static bool got_signal() {
+    return _got_signal;
+}
+
+void init_signal_handler() {
+    struct sigaction act;
+
+    sigemptyset(&act.sa_mask);
+    act.sa_handler = signal_handler;
+    if (sigaction(SIGINT, &act, NULL) == -1) {
+        err(1, "failed to sigaction(SIGINT, ...)");
+    }
+
+    if (sigaction(SIGTERM, &act, NULL) == -1) {
+        err(1, "failed to sigaction(SIGTERM, ...");
+    }
+}
+
+void
+ptrace_loop(pid_t pid, int delay) {
+
+    while(!got_signal()){
+        int status;
+        pid_t tid = waitpid(-1, &status, __WALL);
+        if (WIFEXITED(status)) {
+            warn("tid WIFEXITED: %d", tid);
+            break;
+        }
+
+        thread *t = find_attached_thread(tid);
+        if (t == NULL) {
+            warn("thread is not attached: %d", tid);
+            break;
+        }
+
+        errno = 0;
+        long orig_rax = ptrace(PTRACE_PEEKUSER, t->tid, 8 * ORIG_RAX, NULL);
+        if (orig_rax == -1 && errno) {
+            warn("failed to ptrace(PTRACE_PEEKUSER,...), tid = %d", t->tid);
+            continue;
+        }
+
+        if (ignored_syscall(orig_rax)) {
+            printf("[%d] %s(2)\n", tid, syscall_name(orig_rax));
+        }
+        else {
+            if (t->in_syscall == true) {
+                t->in_syscall = false;
+                //int interval = rand () % delay;
+                printf("[%d] %s(2), usleep = %d\n", t->tid, syscall_name(orig_rax), delay);
+                usleep(delay);
+            }
+            else {
+                t->in_syscall = true;
+            }
+        }
+        ptrace(PTRACE_SYSCALL, t->tid, NULL, NULL);
+    }
+}
+
+int main (int argc, char **argv)
+{
+    pid_t pid;
+    int delay;
+    int long_opt, option_index;
+
+    struct option long_options[] = {
+        {"pid",     required_argument, &long_opt, 'p' },
+        {"delay",   required_argument, &long_opt, 'd' },
+        {"verbose", no_argument,       &long_opt,  0  },
+        {0,         0,                 0,  0 }
+    };
+
+    while (1) {
+        int c = getopt_long(argc, argv, "p:d:v", long_options, &option_index);
+        if (c == -1)
+            break;
+
+        switch (c) {
+        case 0:
+            switch (long_opt) {
+            case 'p':
+                pid = atoi(optarg);
+                break;
+            case 'd':
+                delay = atoi(optarg);
+                break;
+            }
+            break;
+        case 'p':
+            pid = atoi(optarg);
+            break;
+
+        case 'd':
+            delay = atoi(optarg);
+            break;
+        default:
+            printf("?? getopt returned character code 0%o ??\n", c);
+            break;
+        }
+    }
+
+    if (pid == 0) {
+        errx(1, "--pid should be > 0");
+    }
+
+    if (delay <= 0) {
+        errx(1, "--delay should be > 0");
+    }
+
+    init_signal_handler();
+    attach_all_threads(pid);
+    ptrace_loop(pid, delay);
+    detach(pid);
+    exit(0);
+}
